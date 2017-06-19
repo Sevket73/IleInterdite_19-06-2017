f7010bba122137a260db8edcb251ef2f2cf97422
salkims <salkims@pc-dg-039-03>
2017-06-19 14:05:46 +0200

    Merge origin/master
    Conflicts:
    	.gitignore

--- src/Aventuriers/Plongeur.java
+++ src/Aventuriers/Plongeur.java
@@ -20,26 +20,28 @@ public class Plongeur extends Aventurier {
     public Plongeur(String nom, Boolean vivant, int nbAction, Couleur couleur) {
         super(nom, vivant, nbAction, couleur);
     }
-    
-    public ArrayList<Tuiles> deplacementpossible(Grille g){
-        ArrayList<Tuiles>tuilesAdj = new ArrayList();
-        ArrayList<Tuiles>tuilesTest = new ArrayList();
-        
-        tuilesTest = g.getTuilesAdjacentes(positionCourante);
-        for ( Tuiles t :tuilesTest){
-            for (Tuiles t2 : g.getTuilesAdjacentes(t)){
-                if(t2.getEtat()==Assechee){
-                tuilesAdj.add(t2);
-                
-                
-                }else if(t2.getEtat()==Inondee || t2.getEtat()==coulee){
-                    if(!tuilesTest.contains(t2)){
-                    tuilesTest.add(t2);}
-                }
-                
-            }
-        }
-        return tuilesAdj;
-    }
 
+    public ArrayList<Tuiles> deplacementpossible(Grille g) {
+        ArrayList<Tuiles> tuilesAdj = new ArrayList();
+        ArrayList<Tuiles> tuilesTest = new ArrayList();
+
+        tuilesTest=g.getTuilesAdjacentes(positionCourante); 
+        while (!tuilesTest.isEmpty()) {
+            for (Tuiles t : tuilesTest) {
+                if (t.getEtat() == Assechee) {
+                    tuilesAdj.add(t);
+                    
+
+                } else if (t.getEtat() == Inondee || t.getEtat() == coulee) {
+                    if (!tuilesTest.contains(t)) {
+                        tuilesTest.add(t);
+                        tuilesTest.addAll(g.getTuilesAdjacentes(t));
+                    }
+                }
+            }
+            return tuilesAdj;
+
+        }
+    
+}
 }
