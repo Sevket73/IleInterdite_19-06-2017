955b8c60799265eb4b3edb3df51a92bfade69f7f
moi <chaulaic@pc-dg-039-04>
2017-06-21 11:57:09 +0200

    Merge origin/master
    Conflicts:
    	src/Modele/Controleur.java

--- src/Aventuriers/Messager.java
+++ src/Aventuriers/Messager.java
@@ -5,10 +5,36 @@
  */
 package Aventuriers;
 
+import Cartes.CarteTresor;
+import Cartes.CartesTirage;
+import Modele.Couleur;
+import java.util.Scanner;
+
 /**
  *
  * @author chaulaic
  */
-public class Messager {
+public class Messager extends Aventurier {
+    
+    public Messager(String nom, Boolean vivant, int nbAction, Couleur couleur) {
+        super(nom, vivant, nbAction, couleur);
+    }
+    
+    public void donnerCarteTresor(Aventurier j) {
+        if (j instanceof Messager) {
+            System.out.println("Quelle carte souhaitez vous donner ? ");
+        for (CartesTirage c : this.getCartesEnMain()){
+            if (c instanceof CarteTresor) {
+                System.out.println(((CarteTresor) c).getNomTresor());
+            }
+        }
+        String tresor;
+        Scanner rep = new Scanner(System.in);
+        tresor = rep.nextLine();
+        System.out.println("A quel joueur souhaitez vous la donner ? ");
+        }
+        
+        
+    }
     
 }
