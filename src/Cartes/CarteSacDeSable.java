2a253a8fb562b1fad746899bc34328e3c3a74d89
salkims <salkims@pc-dg-039-03>
2017-06-21 13:59:44 +0200

    Merge origin/master
    Conflicts:
    	src/Aventuriers/Aventurier.java

--- src/Cartes/CarteSacDeSable.java
+++ src/Cartes/CarteSacDeSable.java
@@ -5,6 +5,11 @@
  */
 package Cartes;
 
+import Grille.Etat;
+import Grille.Grille;
+import Grille.Tuiles;
+import java.util.ArrayList;
+
 /**
  *
  * @author chaulaic
@@ -14,7 +19,18 @@ public class CarteSacDeSable extends CartesTirage  {
     public CarteSacDeSable() {
         super();
         setNom("SacDeSable");
-    }
-
     
+    }
+    public ArrayList<Tuiles> assechementPossible(Grille g){
+        ArrayList<Tuiles> assPoss = new ArrayList();
+        for (Tuiles t : g.getAze().values()) {
+            if (t.getEtat() != Etat.Inondee) {
+                assPoss.add(t);
+            } 
+        }  
+        
+    return assPoss;
+
+    }
 }
+
