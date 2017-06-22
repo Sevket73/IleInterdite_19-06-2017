97c7ebd2836c97516aa64126d54bc391c1baf5a6
salkims <salkims@pc-dg-039-03>
2017-06-22 14:59:03 +0200

    Merge origin/master
    Conflicts:
    	src/Aventuriers/Aventurier.java

--- src/Cartes/CarteHelicoptere.java
+++ src/Cartes/CarteHelicoptere.java
@@ -18,38 +18,44 @@ public class CarteHelicoptere extends CartesTirage {
     public CarteHelicoptere() {
         super();
         setNom("helicoptere");
-        
+
     }
 
     @Override
-    public void deplacer1Joueur(Aventurier j1,String tuile,Grille g){
-            Tuile t1 = g.getTuiles(tuile);
+    public void deplacer1Joueur(Aventurier j1, String tuile, Grille g) {
+        Tuile t1 = g.getTuiles(tuile);
+        j1.setPositionCourante(t1.getCoordonnée().getColonne(), t1.getCoordonnée().getLigne());
+    }
 
-        j1.setPositionCourante(t1.getCoordonnée().getColonne(),t1.getCoordonnée().getLigne());
-    }
     @Override
-    public void deplacer2Joueurs(Aventurier j1,Aventurier j2,String tuile,Grille g){
+    public void deplacer2Joueurs(Aventurier j1, Aventurier j2, String tuile, Grille g) {
         Tuile t1 = g.getTuiles(tuile);
-        j1.setPositionCourante(t1.getCoordonnée().getColonne(),t1.getCoordonnée().getLigne());
-        j2.setPositionCourante(t1.getCoordonnée().getColonne(),t1.getCoordonnée().getLigne());
+        if ((j1.getPositionCourante() == j2.getPositionCourante())) {
+            j1.setPositionCourante(t1.getCoordonnée().getColonne(), t1.getCoordonnée().getLigne());
+            j2.setPositionCourante(t1.getCoordonnée().getColonne(), t1.getCoordonnée().getLigne());
+        }
     }
+
     @Override
-    public void deplacer3Joueurs(Aventurier j1,Aventurier j2,Aventurier j3,String tuile,Grille g){
+    public void deplacer3Joueurs(Aventurier j1, Aventurier j2, Aventurier j3, String tuile, Grille g) {
         Tuile t1 = g.getTuiles(tuile);
-        j1.setPositionCourante(t1.getCoordonnée().getColonne(),t1.getCoordonnée().getLigne());
-        j2.setPositionCourante(t1.getCoordonnée().getColonne(),t1.getCoordonnée().getLigne());
-        j3.setPositionCourante(t1.getCoordonnée().getColonne(),t1.getCoordonnée().getLigne());
+        if ((j1.getPositionCourante() == j2.getPositionCourante()) && (j2.getPositionCourante() == j3.getPositionCourante())) {
+            j1.setPositionCourante(t1.getCoordonnée().getColonne(), t1.getCoordonnée().getLigne());
+            j2.setPositionCourante(t1.getCoordonnée().getColonne(), t1.getCoordonnée().getLigne());
+            j3.setPositionCourante(t1.getCoordonnée().getColonne(), t1.getCoordonnée().getLigne());
+        }
     }
+
     @Override
-    public void deplacer4Joueurs(Aventurier j1,Aventurier j2,Aventurier j3,Aventurier j4,String tuile,Grille g){
+    public void deplacer4Joueurs(Aventurier j1, Aventurier j2, Aventurier j3, Aventurier j4, String tuile, Grille g) {
         Tuile t1 = g.getTuiles(tuile);
         if ((j1.getPositionCourante() == j2.getPositionCourante()) && (j2.getPositionCourante() == j3.getPositionCourante()) && (j3.getPositionCourante() == j4.getPositionCourante())) {
-            
+            j1.setPositionCourante(t1.getCoordonnée().getColonne(), t1.getCoordonnée().getLigne());
+            j2.setPositionCourante(t1.getCoordonnée().getColonne(), t1.getCoordonnée().getLigne());
+            j3.setPositionCourante(t1.getCoordonnée().getColonne(), t1.getCoordonnée().getLigne());
+            j4.setPositionCourante(t1.getCoordonnée().getColonne(), t1.getCoordonnée().getLigne());
         }
-        j1.setPositionCourante(t1.getCoordonnée().getColonne(),t1.getCoordonnée().getLigne());
-        j2.setPositionCourante(t1.getCoordonnée().getColonne(),t1.getCoordonnée().getLigne());
-        j3.setPositionCourante(t1.getCoordonnée().getColonne(),t1.getCoordonnée().getLigne());
-        j4.setPositionCourante(t1.getCoordonnée().getColonne(),t1.getCoordonnée().getLigne());
+
     }
-    
+
 }
